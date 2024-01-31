b660266e0050eace916bc4c0a8294129c03ec840
selkovevgeny <selkov.evgeny@yandex.ru>
2024-01-31 08:47:59 +0300

    Добавлен setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='relaylab',
+   version='0.1.0',
+   description='Библиотека предназначенная для простой работы с аварийными осциллограммами в формате COMTRADE в jupiter notebook',
+   author='selkovevgeny',
+   author_email='selkov.evgeny@yandex.ru',
+   packages=['relaylab'],  #same as name
+   install_requires=['plotly', 'numpy', 'pandas'], #external packages as dependencies
+)
